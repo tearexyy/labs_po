c29487d64956972b2ab9db5932747630ac6b4bfd
tearexyy <d.v.mokrenko@gmail.com>
2026-05-10 15:32:56 +0300

    Remove build folder from tracking

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/cmake/lab2
-# Build directory: /mnt/c/cmake/lab2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
--- build/tests/CMakeFiles/test_base32file.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test_base32file.
-# This may be replaced when dependencies are built.
